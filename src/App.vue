9164696c388f58d5b1d62bd4e5a83b067f9a0cb6
Alang Kim <rlaangh77@naver.com>
2021-10-03 14:41:57 +0900

    📄 docs : markup 작성

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
-  <RouterView />
+  <div class="app__inner">
+    <div class="app__title"></div>
+    <div class="app__todo-list">
+      <div class="todo-list__form">
+        <form @submit.prevent>
+          <input
+            type="text"
+            placeholder="할일을 입력하세요">
+          <button> 등록! </button>
+        </form>
+      </div>
+      <div class="todo-list__lists">
+        <ul>
+          <li>할일 1</li>
+          <li>할일 2</li>
+        </ul>
+      </div>
+      <div class="todo-list__count">
+        <span class="count__done">0</span>
+        <span class="divider">/</span>
+        <span class="count__total">2</span>
+      </div>
+    </div>
+  </div>
 </template>
--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'vuex';
-import message from './message';
-export default createStore({
-  modules : {
-    message
-  }
-});
